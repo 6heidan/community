c60d9858906184ab71a4ddb70fa368a71a8f5037
heidan <1321016794@qq.com>
2020-04-25 19:43:13 +0800

    extract github config from code to properties

--- src/main/java/com/heidan/community/controller/AuthorizeController.java
+++ src/main/java/com/heidan/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.heidan.community.dto.AccessTokenDTO;
 import com.heidan.community.dto.GithubUser;
 import com.heidan.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("Iv1.b798b2323a6a8215");
-        accessTokenDTO.setClient_secret("09f9de0f32bd274119e2e05fc7919cba6e249b0b");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
