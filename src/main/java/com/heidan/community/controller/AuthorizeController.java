fd8f97179c0ed8abe06cb736b63c5f507419c0ee
heidan <1321016794@qq.com>
2020-04-25 17:09:35 +0800

    one

---
+++ src/main/java/com/heidan/community/controller/AuthorizeController.java
@@ -0,0 +1,29 @@
+package com.heidan.community.controller;
+
+import com.heidan.community.dto.AccessTokenDTO;
+import com.heidan.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code")String code,
+                           @RequestParam(name="state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("Iv1.b798b2323a6a8215");
+        accessTokenDTO.setClient_secret("09f9de0f32bd274119e2e05fc7919cba6e249b0b");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        githubProvider.getAccessToken(accessTokenDTO);
+        return "index";
+    }
+
+}
